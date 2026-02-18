465cfde9fb40034a5652fde1c7ad651e75d323b3
cturel <cturel28@icloud.com>
2026-02-18 13:19:33 +0000

    git add app.py git commit -m "feat: configure database connection using environment variables"

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+# Hard Requirement: Read DB config from environment variables
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
-if __name__ == "__main__":
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
     app.run()
